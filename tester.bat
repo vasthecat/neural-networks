c6a9ddddb9cd8bede7906dfea73d1e9b8b942903
Andrew Guschin <guschin.drew@gmail.com>
2023-10-28 13:35:37 +0400

    Исправлена ошибка в файле тестера

--- tester.bat
+++ tester.bat
@@ -1,4 +1,4 @@
-.\nntask1.exe --input1 tests\t1_input.txt --output t1_output.xml
+.\nntask1.exe --input1 tests\t1_input.txt --output1 t1_output.xml
 
-.\nntask2.exe --input1 tests\t2_input.xml --output t2_output1.xml
-.\nntask2.exe --input1 tests\t2_input_cycle.xml --output t2_output2.xml
+.\nntask2.exe --input1 tests\t2_input.xml --output1 t2_output1.xml
+.\nntask2.exe --input1 tests\t2_input_cycle.xml --output1 t2_output2.xml
