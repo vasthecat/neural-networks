589c45f6e032dac7e05132070433fce7f1ecb0e9
Andrew Guschin <guschin.drew@gmail.com>
2023-10-28 13:31:39 +0400

    Добавлен файл для запуска тестов

---
+++ tester.bat
@@ -0,0 +1,4 @@
+.\nntask1.exe --input1 tests\t1_input.txt --output t1_output.xml
+
+.\nntask2.exe --input1 tests\t2_input.xml --output t2_output1.xml
+.\nntask2.exe --input1 tests\t2_input_cycle.xml --output t2_output2.xml
